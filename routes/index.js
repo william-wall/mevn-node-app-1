7effd09e8d69f644b8903f88b8d04caa0a6cb90e
william-wall <william@williamwall.ie>
2018-07-24 17:26:09 +0100

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'DonationWeb App Lab 1' });
+
+});
+
+module.exports = router;
+
+
